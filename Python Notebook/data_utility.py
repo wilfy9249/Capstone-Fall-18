9aac6a973a954597622190e72aafbf12c494ed69
Pooja Suvarna <ps834@njit.edu>
2018-10-24 20:54:57 -0400

    Resolved Data Utility functions

--- Python Notebook/data_utility.py
+++ Python Notebook/data_utility.py
@@ -1,14 +1,10 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
-
 
 import numpy as np
 import pandas as pd
 
-
-# In[2]:
 
 
 #function to get current directory
@@ -17,21 +13,11 @@ def getCurrentDirectory():
     print(os.listdir('../'))
 
 
-# In[3]:
-
 
 #function to read csv file
 def readCSVfile(path):
     crimes_original = pd.read_csv(path, low_memory=False)
 
-
-# In[5]:
-
-
-
-
-
-# In[ ]:
 
 
 
