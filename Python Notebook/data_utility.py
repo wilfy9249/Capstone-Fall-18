207445c8b40c85ae22143a1cab69ff4cbe819ba3
Pooja Suvarna <ps834@njit.edu>
2018-10-26 04:21:07 -0400

    Modified Sort and getCount

--- Python Notebook/data_utility.py
+++ Python Notebook/data_utility.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+# In[1]:
+
 
 import numpy as np
 import pandas as pd
 
+
+# In[2]:
 
 
 #function to get current directory
@@ -13,11 +17,44 @@ def getCurrentDirectory():
     print(os.listdir('../'))
 
 
+# In[3]:
+
 
 #function to read csv file
 def readCSVfile(path):
     crimes_original = pd.read_csv(path, low_memory=False)
+    return crimes_original
 
+
+# In[4]:
+
+
+#function to filter Data
+def filterData(data,column,value):
+    filterData = data.loc[data[column] == value]
+    return filterData
+    
+
+
+# In[5]:
+
+
+#function to get count of a value
+def getCount(data,column,columnName):    
+    data_count = pd.DataFrame({columnName:data.groupby(column).size()}).reset_index()
+    return data_count
+
+
+# In[7]:
+
+
+#function to sort
+def sortValue(data,column,ascBoolean):
+    sorted_data = data.sort_values(column,ascending = ascBoolean)
+    return sorted_data
+
+
+# In[ ]:
 
 
 
